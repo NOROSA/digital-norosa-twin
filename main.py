ad3c5dbc723066d77fefd51efa0a1430b154a3f5
NOROSA <norosanorosez@gmail.com>
2025-06-07 22:18:16 +0000

    go drop pending updates

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_msg))
 
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
