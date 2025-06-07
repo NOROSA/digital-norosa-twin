489ca70826039fd78366ecb3a15c1ba9c9a3f05c
NOROSA <norosanorosez@gmail.com>
2025-06-07 21:42:34 +0000

    removed main

---
+++ agent/__init__.py
@@ -0,0 +1,11 @@
+"""
+Paquete agent
+~~~~~~~~~~~~~
+
+Inicializa el agente y expone utilidades principales.
+"""
+
+from .builder import build_agent
+
+__all__ = ["build_agent"]
+__version__ = "0.1.0"
